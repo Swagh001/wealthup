0dc673c907322842d3c0878b842d92b628cc5196
shailesh wagh <shaileshwagh300@gmail.com>
2024-01-26 12:28:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import css from "./App.css";
+import img1 from "./photos/1 (1) 2.png"
+import Line from "./photos/Line 1.png"
+import Mobile from "./photos/Mobile.png"
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div id="container-first">
+          <div><h1 id="title">Check your financial health</h1>
+          </div>
+          <div className="container-subT"><h1 h1 id="sub-title">Use WeathoMeter to get a free report
+            card for your finances- within minutes!</h1>
+          </div>
+          <div >
+            <button id="getStart">Get Started</button>
+          </div>
+        </div>
+
+        <div id="conatiner-second" >
+          <div>
+            <div>
+              <div>
+                <div className="sub-cont">
+                  <div> 
+                    <img
+                      src={img1}
+                      alt="My Image"
+                    />
+                  </div>
+                  <div>Expected Retirement Age</div>
+                </div>
+                <div className="sub-cont">
+                  <div style={{ marginRight: '41px' }}></div>
+                  <div>
+                    <img
+                      src={Line}
+                      alt="line1"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <div className="sub-cont">
+                  <div> 
+                    <img
+                      src={img1}
+                      alt="My Image"
+                    />
+                  </div>
+                  <div>Expected Retirement Age</div>
+                </div>
+                <div className="sub-cont">
+                  <div style={{ marginRight: '41px' }}></div>
+                  <div>
+                    <img
+                      src={Line}
+                      alt="line1"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div> 
+              <img
+                src={Mobile}
+                alt="mobile"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div> 
+                    <img
+                      src={img1}
+                      alt="img3"
+                    />
+                  </div>
+                  <div>Expected Retirement Age</div>
+                </div>
+                <div>
+                  <div></div>
+                  <div>
+                    <img
+                      src={Line}
+                      alt="line1"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+            <div>
+                <div>
+                  <div> 
+                    <img
+                      src={img1}
+                      alt="img4"
+                    />
+                  </div>
+                  <div>Expected Retirement Age</div>
+                </div>
+                <div>
+                  <div></div>
+                  <div>
+                    <img
+                      src={Line}
+                      alt="line1"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+
+
+
+      <div>2</div>
     </div>
   );
 }
